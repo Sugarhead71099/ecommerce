a4e4988e9ca73b81501eac8aebc50dceea09f5ea
Braijon Glover <braijonglover3@gmail.com>
2018-03-21 11:49:53 -0500

    Create thank_you.php file inside public directory

---
+++ public/thank_you.php
@@ -0,0 +1,16 @@
+<?php
+
+	require_once("../resources/config.php");
+	include(TEMPLATE_FRONT . DS . "header.php");
+
+	process_transaction();
+
+?>
+
+    <!-- Page Content -->
+    <div class="container">
+    	<h1 class="text-center">THANK YOU</h1>
+    </div>
+    <!-- /.container -->
+
+<?php include(TEMPLATE_FRONT . DS . "footer.php"); ?>
